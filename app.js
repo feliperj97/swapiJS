563948a8269c20df5952e50ca8f94aacd50e3b68
Felipe Lamim <fclamim@gmail.com>
2018-10-18 15:43:32 -0300

    retornando nome, clima e terreno

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 let button = document.querySelector('#button');
 let name = document.querySelector('#name');
+let climate = document.querySelector('#climate');
+let terrain = document.querySelector('#terrain');
 
 function getInfo(){
 
     let randomNumber = Math.floor((Math.random() * 50) + 1)
     let apiUrl = 'https://swapi.co/api/planets/' + randomNumber;
-    axios.get(apiUrl).then(function(response){
+    axios.get(apiUrl).then(response=>{
         updateInfo(response.data);
+    }).catch(e => {
+        console.log("Erro");
     })
 }
 
 function updateInfo(data){
     name.innerText = data.name;
+    climate.innerText = data.climate;
+    terrain.innerText = data.terrain;
 }
 
 button.addEventListener('click', getInfo);
